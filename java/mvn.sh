d7665fec0a50a1b7bf2fbd71ff1688b7d07db08a
yckim <ychangkim@crossent.com>
2017-09-07 15:23:17 +0900

    build

---
+++ java/mvn.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e -x -u
+
+echo $(pwd)
+
+if [ $TEST_SKIP = "yes" ]; then
+    skip="true"
+else
+    skip="false"
+fi
+
+mvn -s /usr/share/maven/ref/settings-docker.xml -f git/pom.xml -DskipTests=$skip $GOAL
+
